cdb82430fa94e67473335ce5337e032e0c1dc5e5
Moderr <tymonek12345@gmail.com>
2024-01-02 22:38:17 +0100

    Premake fix

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,43 @@
 project "imgui"
-    kind "StaticLib"
-    language "C++"
+	kind "StaticLib"
+	language "C++"
 
-    targetdir ("Binaries/" .. OutputDir .. "/%{prj.name}")
-    objdir    ("Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+	targetdir("%{wks.location}/Binaries/" .. OutputDir .. "/%{prj.name}")
+  	objdir("%{wks.location}/Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
-    files
-    {
-        "imconfig.h",
-        "imgui.h",
-        "imgui.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_widgets.cpp",
-        "imgui_tables.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h"
-    }
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
+	}
 
-    filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
+	includedirs
+	{
+		".",
+		"backends/"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
